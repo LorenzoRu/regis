3a01ccd32ce890473157aedfa60b2ba5d4037db1
LorenzoRU <Lorenzo.ruocco9219@gmail.com>
2025-06-09 19:05:33 +0200

    add css style

--- modules/dashboard_front/src/main.jsx
+++ modules/dashboard_front/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './assets/styles/base.css';
+import './assets/styles/variables.css';
+import './assets/styles/components.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
